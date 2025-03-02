bf5f743a6f0f6a9354ea8d931897bf3abc016cc9
Muneer <muneerskoppam1107@gmail.com>
2025-03-02 14:19:47 +0530

    backend conn

--- dist/app.js
+++ dist/app.js
@@ -24,7 +24,8 @@ const feedBackRoutes_1 = __importDefault(require("./routes/feedBackRoutes"));
 const messageRoutes_1 = __importDefault(require("./routes/messageRoutes"));
 // import messageRouter from './routes/messageRoutes';
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: https://2wheeleeee.vercel.app/ || process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "https://2wheeleeee.vercel.app" || "http://localhost:5173",
     credentials: true,
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
