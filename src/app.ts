9e07c704f8e265833c246483903b306957a8a286
Muneer <muneerskoppam1107@gmail.com>
2025-03-02 14:18:51 +0530

    backend

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,8 @@ import messageRouter from './routes/messageRoutes';
 
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  // origin: https://2wheeleeee.vercel.app/ || process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "https://2wheeleeee.vercel.app" || "http://localhost:5173",
   credentials: true,
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
